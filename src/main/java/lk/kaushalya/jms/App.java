fa4f57e6f95bebae5233ab357637b6e8ce6f8bd7
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-06-12 00:46:10 +0530

    Here is a commit message summarizing the changes in the provided diff:
    Add main application class for JMS Producer.
    
    Creates `App.java` to act as the JMS message publisher (SecondClient). It connects to the message broker using JNDI to look up `myTopicConnectionFactory` and `myTopic`, and sets up a `TopicPublisher`. Implements a console interface using a `Scanner` loop to continuously read user input and publish it as `TextMessage`s to the topic until the user types 'exit'.

---
+++ src/main/java/lk/kaushalya/jms/App.java
@@ -0,0 +1,42 @@
+package lk.kaushalya.jms;
+
+import jakarta.jms.*;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            InitialContext ic = new InitialContext();
+
+            TopicConnectionFactory factory = (TopicConnectionFactory) ic.lookup("myTopicConnectionFactory");
+            TopicConnection connection = factory.createTopicConnection();
+            connection.start();
+
+            TopicSession session = connection.createTopicSession(false, TopicSession.AUTO_ACKNOWLEDGE);
+            Topic topic = (Topic) ic.lookup("myTopic");
+            TopicPublisher publisher = session.createPublisher(topic);
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter message or type 'exit' to quit:");
+
+            while(true){
+                String line = sc.nextLine();
+                if(line.equalsIgnoreCase("exit")){
+                    break;
+                }
+
+                TextMessage txtMessage = session.createTextMessage();
+                txtMessage.setText(line);
+
+                publisher.publish(txtMessage);
+            }
+
+
+        } catch (NamingException | JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
